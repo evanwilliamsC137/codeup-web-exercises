7a63e3f02dcadcdfeb26ebb7a2e0c6b61b26adb1
Evan Williams <evanwilliams@Evans-MacBook-Pro.local>
2021-03-25 16:39:40 -0500

    finished while and do while exercise

--- js/while.js
+++ js/while.js
@@ -1,12 +1,47 @@
-// var counter = 8;
-// while (counter >= 0) {
-//     console.log(counter);
-//     counter = counter - 1;
+
+//while loop
+// var whileInput = 2;
+// while (whileInput <= 65536) {
+//     console.log(whileInput);
+//     whileInput = whileInput * 2;
 // }
 
+//do-while loop
+// var number = Math.floor(Math.random() * 6) + 1;
+// var guess;
+//
+// do {
+//     guess = parseInt(prompt("Enter a number between 1 and 6"));
+// }while (guess !== number) {
+//     alert("your guess of " + guess + " matches the number" + number);
+// }
 
-var whileInput = 2;
-while (whileInput <= 65536) {
-    console.log(whileInput);
-    whileInput = whileInput * 2;
-}
+//ice cream loop
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+//first attempt
+// do {
+//     var conesSold = Math.floor(Math.random() * 5) + 1;
+//     console.log("you sold " + conesSold + " cones");
+//     allCones = allCones - conesSold;
+//     console.log(allCones);
+//     if ((conesSold >= allCones) && (allCones > 0)) {
+//         console.log("We dont have that many cones!");
+//     }
+//
+// } while (allCones > 0) {
+//     console.log("we sold all the cones!");
+//
+// }
+
+// walkthrough
+do {
+    var conesBought = Math.floor(Math.random() * 5) + 1;
+    if (conesBought > allCones) {
+        console.log("I cant sell " + conesBought + " I only have  " + allCones + " left sorry");
+    } else {
+        allCones = allCones - conesBought;
+        console.log("Cones Sold " + conesBought + " I have " + allCones + " left to sell");
+    }
+} while (allCones > 0);
+console.log(allCones + " cones left!");
