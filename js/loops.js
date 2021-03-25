7c53e31a82627e61dfb55e292e25c91e6211b3c4
Evan Williams <evanwilliams@Evans-MacBook-Pro.local>
2021-03-25 11:41:01 -0500

    update on loops

---
+++ js/loops.js
@@ -0,0 +1,49 @@
+console.log("hello world");
+
+//While loops
+
+//pizza loop
+
+// var slices = 8;
+// while (slices > 0) {
+//     console.log("I'll have a slice");
+//     slices = slices - 1;
+//     console.log("There are " + slices + " Left");
+// }
+
+// Pseudocode
+
+// Ask for user input
+// Get the user input
+// Declare a user input variable
+// Store user input in the variable
+// Declare a variable to store the total (accumulator)
+// Add cost of new item to the total
+// Keep storing the totals in the accumulator
+// Tell the user how to end the program (sentinel value)
+// Loop back around until user tells to stop
+
+// var priceOfItem = parseFloat(prompt("Enter the price of the first item"));
+// alert("The price of your item was " + priceOfItem);
+//
+// var totalCost = priceOfItem;
+// var userInput = "";
+// // create a sentinel value
+//
+// while (userInput !== "STOP" ) {
+//     userInput = prompt("Enter the price of your next item \r\n\ Enter STOP when you are done");
+//     if (userInput === "STOP") {
+//         alert("OK. your final total is " + totalCost);
+//     }else{
+//         priceOfItem = parseFloat(userInput);
+//         totalCost = totalCost + priceOfItem;
+//         alert("Your total is now " + totalCost.toFixed(2));
+//     }
+// }
+
+// Pure math example
+// var counter = 8;
+// while (counter >= 0) {
+//     console.log(counter);
+//     counter = counter - 1;
+// }
---
+++ js/while.js
@@ -0,0 +1,12 @@
+// var counter = 8;
+// while (counter >= 0) {
+//     console.log(counter);
+//     counter = counter - 1;
+// }
+
+
+var whileInput = 2;
+while (whileInput <= 65536) {
+    console.log(whileInput);
+    whileInput = whileInput * 2;
+}
