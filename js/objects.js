02eacfd27e39cbc1714ca2c787de2be93fb474e6
Evan Williams <evanwilliams@Evans-MacBook-Pro.local>
2021-04-01 14:26:23 -0500

    attempting object exercises

--- js/objects.js
+++ js/objects.js
@@ -28,7 +28,7 @@ console.log("Sup objects");
     }
     console.log(person.firstName);
     console.log(person.lastName);
-    console.log(person.sayHello());
+
 
 
     /**
@@ -40,6 +40,9 @@ console.log("Sup objects");
      * Example
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
+
+    console.log(person.sayHello());
+
 
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
@@ -55,12 +58,65 @@ console.log("Sup objects");
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320},
+    ];
 
+    //with for-loop
+    // function discount(x) {
+    //     for(var i = 0; i < x.length; i++) {
+    //         shoppers.forEach(function (name, amount) {
+    //             if (x[i].amount > 200) {
+    //                 console.log(x[i].name + " gets a discount of " + (x[i].amount / 12).toFixed(2));
+    //             } else {
+    //                 console.log(x[i].name + " gets no discount ");
+    //             }
+    //         })
+    //     }
+    // }
+    // discount(shoppers);
+
+    //creating local variables to compare to
+    function discountApply(x) {
+        var minSpend = 200;
+        var discAmount = .12;
+        shoppers.forEach(function (name, i) {
+
+            if (x[i].amount > minSpend) {
+
+                console.log(x[i].name + " spent $" + (x[i].amount).toFixed(2) + " and gets a discount of $" + (x[i].amount * discAmount).toFixed(2) + ". Bringing your total owed to $" + (x[i].amount - (x[i].amount * discAmount)).toFixed(2));
+
+            } else {
+
+                console.log(x[i].name + " spent $" + (x[i].amount).toFixed(2) + " Which brings him $" + ((x[i].amount - minSpend) * -1).toFixed(2) + " short of a discount. Your total you owe is $" + (x[i].amount).toFixed(2));
+
+            }
+        })
+    }
+
+    discountApply(shoppers);
+
+    //first attempt incomplete
+
+    // function discount(shoppers) {
+    //     var shoppers = [
+    //         {name: 'Cameron', amount: 180},
+    //         {name: 'Ryan', amount: 250},
+    //         {name: 'George', amount: 320}
+    //     ];
+    //
+    //         if (shoppers[0].amount <= 200) {
+    //             console.log(shoppers[0].name + " gets a discount of " + (shoppers[0].amount/12));
+    //         }else {
+    //             console.log(shopper[0].name + " gets no discount");
+    //         }
+    // }
+    //
+    // console.log(discount('Cameron'));
+    // console.log(discount('Ryan'));
+    // console.log(discount('George'));
     /** TODO:
      * Create an array of objects that represent books and store it in a
      * variable named `books`. Each object should have a title and an author
@@ -73,6 +129,33 @@ console.log("Sup objects");
      * > console.log(books[0].author.firstName) // "Douglas"
      * > console.log(books[0].author.lastName) // "Adams"
      */
+
+    var books = [
+        {title: 'Call of Cthulhu', author: {firsName: 'HP', lastName: 'Lovecraft'}},
+        {title: 'Fight Club', author: {firsName: 'Chuck', lastName: 'Palahniuk'}},
+        {title: 'Beyond Good and Evil', author: {firsName: 'Friedrich', lastName: 'Nietzsche'}},
+        {title: 'The Prince', author: {firsName: 'Nicolo', lastName: 'Machiavelli'}},
+        {title: 'Alices adventures in Wonderland', author: {firsName: 'Lewis', lastName: 'Carroll'}}
+    ]
+
+    // console.log(books[0].title);
+    // console.log(books[0].author.firsName);
+    // console.log(books[0].author.lastName);
+    // console.log(books[1].title);
+    // console.log(books[1].author.firsName);
+    // console.log(books[1].author.lastName);
+    // console.log(books[2].title);
+    // console.log(books[2].author.firsName);
+    // console.log(books[2].author.lastName);
+    // console.log(books[3].title);
+    // console.log(books[3].author.firsName);
+    // console.log(books[3].author.lastName);
+    // console.log(books[4].title);
+    // console.log(books[4].author.firsName);
+    // console.log(books[4].author.lastName);
+
+
+
 
     /**
      * TODO:
@@ -99,6 +182,22 @@ console.log("Sup objects");
      *      ...
      */
 
+    // books.forEach(function(book, index){
+//     console.log("Book # "+ (index + 1));
+//     console.log("Title: " + book.title);
+//     console.log("Author: " + book.author.firstName + " " + book.author.lastName);
+//     console.log("---")
+// })
+    books.forEach(function(book, index) {
+        console.log("Book # " + (index + 1));
+        showBookInfo(book);
+        console.log("---")
+    });
+    function showBookInfo(book) {
+        console.log("Title: " + book.title);
+        console.log("Author: " + book.author.firsName + " " + book.author.lastName);
+    }
+
     /**
      * Bonus:
      * - Create a function named `createBook` that accepts a title and author
@@ -109,5 +208,18 @@ console.log("Sup objects");
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
      */
+    function createBook(title, author) {
+        var authorName = author.splice(" ")
+        return {
+            title: title,
+            author: {
+                firsName: authorName[0],
+                lastName: authorName[authorName.length - 1]
+            }
+        }
+        console.log("Title: " + title.title);
+        console.log("Author: " + author.author);
+    }
+
 
 })();
