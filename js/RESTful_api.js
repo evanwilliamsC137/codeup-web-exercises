68aee2b1ccef97cdfec1610ad8e9945726a31049
Evan Williams”git config --global user.nameclear <evan.b.williams95@gmail.com>
2021-05-18 14:50:17 -0500

    finished restful api lecture

--- js/RESTful_api.js
+++ js/RESTful_api.js
@@ -26,7 +26,7 @@ fetch("https://alkaline-aluminum-bulb.glitch.me/books/1", getOptions)
 
 
 
-//post
+// post
 
 let newBook = {
     "title": "Fight Club",
@@ -44,46 +44,101 @@ let postOptions = {
     body: JSON.stringify(newBook)
 };
 
-let modification = {
-    "title": "Fight Club",
-    "author": {
-        "firstName": "Chuck",
-        "lastName": "Paulanick"
-    }
-}
-
-let putOptions = {
-    method: 'PUT',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(modification)
-};
-
-
-
 $('#btn').click(() => {
-    fetch("https://alkaline-aluminum-bulb.glitch.me/books", postOptions)
-        // .then(resp => resp.json())
-        .then(getBooks);
+    fetch("https://alkaline-aluminum-bulb.glitch.me/books")
+        .then(resp => resp.json())
+        .then(books => {
+            for (let book of books) {
+                if (book.title !== newBook.title || book.author.firstName !== newBook.author.firstName || book.author.lastName !== newBook.author.lastName) {
+                    fetch("https://alkaline-aluminum-bulb.glitch.me/books", postOptions)
+                        // .then(resp => resp.json())
+                        .then(getBooks)
+                        .then(console.log(books))
+                }else {
+                    alert("hey, that book already exists!");
+                    break;
+                }
+            }
+        })
+
 })
 
-//PUT
-fetch("https://alkaline-aluminum-bulb.glitch.me/books/6", putOptions).then(getBooks);
+// PUT
+// let putThis = {
+//     "title": "Fight Club",
+//     "author": {
+//         "firstName": "Chuck",
+//         "lastName": "Paulanick"
+//     }
+// }
+//
+// let putOptions = {
+//     method: 'PUT',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(putThis)
+// };
+//
+//
+// fetch("https://alkaline-aluminum-bulb.glitch.me/books/6", putOptions).then(getBooks);
 
-//PATCH
-let patchThis = {
-    "title": "Percy Jackson & the titan's curse"
-}
 
-let patchOptions = {
-    method: 'PATCH',
+// PATCH
+// let patchThis = {
+//     "title": "Percy Jackson & the titan's curse"
+// }
+//
+// let patchOptions = {
+//     method: 'PATCH',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(patchThis)
+// };
+//
+// fetch("https://alkaline-aluminum-bulb.glitch.me/books/7", patchOptions).then(getBooks);
+
+
+
+// DELETE
+
+let deleteOptions = {
+    method: 'DELETE',
     headers: {
         'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(patchThis)
+    }
 };
 
-fetch("https://alkaline-aluminum-bulb.glitch.me/books/7", patchOptions).then(getBooks);
+
+
+// $('#removeBtn').click(() => {
+//     fetch("https://alkaline-aluminum-bulb.glitch.me/books")
+//         .then(resp => resp.json())
+//         .then(books => {
+//             let removeBooks = [];
+//             for (let book of books) {
+//                 if (removeBooks.length === 0){
+//                     removeBooks.push(book);
+//                     continue;
+//                 }
+//                 for (let existingBook of removeBooks) {
+//                     if (book.title !== existingBook.title || book.author.firstName !== existingBook.author.firstName || book.author.lastName !== existingBook.author.lastName) {
+//                         removeBooks.push(book);
+//                     }else {
+//                         fetch(`https://alkaline-aluminum-bulb.glitch.me/books/${book.id}`, deleteOptions).then(getBooks);
+//                     }
+//                 }
+//             }
+//         })
+//
+//
+// })
+
+$("#removeBtn").click(() => {
+    let inputVal = $('#id-to-delete').val();
+    fetch(`https://alkaline-aluminum-bulb.glitch.me/books/${inputVal}`, deleteOptions)
+        .then(getBooks)
+})
 
 getBooks();
