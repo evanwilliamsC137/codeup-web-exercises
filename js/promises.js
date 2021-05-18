0f565398ec964afc6c0697889421de2dc8890725
Evan Williams”git config --global user.nameclear <evan.b.williams95@gmail.com>
2021-05-17 20:51:10 -0500

    finished wait function

--- js/promises.js
+++ js/promises.js
@@ -41,3 +41,18 @@ function getGithubUsernames(userName) {
     }))
 }
 getGithubUsernames("evanwilliamsC137")
+
+function wait(num) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.1) {
+                resolve('Here is your data: ...');
+            } else {
+                reject('Network Connection Error!');
+            }
+        }, num);
+    });
+}
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
