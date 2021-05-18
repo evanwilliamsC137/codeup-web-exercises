db6b16e87be9e30e9fc1c08c8266da770f95382b
Evan Williams”git config --global user.nameclear <evan.b.williams95@gmail.com>
2021-05-18 10:14:47 -0500

    finished promises walkthrough

--- js/promises.js
+++ js/promises.js
@@ -1,58 +1,33 @@
 
-
-// function githubActivity(username) {
-//     fetch('https://api.github.com/users', {headers: {'Authorization': GITHUB_TOKEN}})
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data)
-//         })
-//         .catch(error => console.log(error))
-// }
-// console.log(githubActivity('evan-williams'))
-
-// var test = fetch("https://api.github.com/users/evan-williams/events", {
-//     headers: {'Authorization': GITHUB_TOKEN}
-//
-// }).then(function(data){
-//     console.log(data)
-//     return data.json()
-// }).then(function(data){
-//     console.log(data)
-// })
-//
-
-
-// function getGithubUsernames(userName) {
-//     return fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': GITHUB_TOKEN}})
-// .then(response => response.json()).then(data => {
-//         console.log(data);
-//     })
-// }
-// getGithubUsernames();
-
 function getGithubUsernames(userName) {
-    return fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': 'GITHUB_ACCESS_TOKEN'}})
+    return fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': 'GITHUB_TOKEN'}})
 .then(response => response.json().then(data => {
         console.log(data);
-        var latestPush = data[0].created_at
-        let date = new Date(latestPush);
-
-        console.log(date.toString());
+        let lastPush;
+        for (let event of data) {
+            if (event.type === "PushEvent") {
+                lastPush = new Date(event.created_at);
+                break;
+            }
+        }
+        console.log(lastPush.toDateString());
     }))
 }
 getGithubUsernames("evanwilliamsC137")
 
+//long hand
 function wait(num) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
-            if (Math.random() > 0.1) {
-                resolve('Here is your data: ...');
-            } else {
-                reject('Network Connection Error!');
-            }
+                resolve(`You'll see this after ${num/1000} seconds`);
         }, num);
     });
 }
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+//short hand
+// function wait(num) {
+//     return new Promise(resolve => setTimeout(resolve, num));
+// }
+
+wait(1000).then((message) => console.log(message));
+wait(3000).then((message) => console.log(message));
