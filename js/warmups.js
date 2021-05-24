8ec97fb4a9cd1d326af35b608b4e78652615e146
Evan Williams”git config --global user.nameclear <evan.b.williams95@gmail.com>
2021-05-24 09:30:11 -0500

    completed fizzbuzz exercise

---
+++ js/warmups.js
@@ -0,0 +1,33 @@
+// function fizzBuzz() {
+//     for (var i = 0; i <= 30; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("Fizz");
+//         }else if (i % 3 === 0) {
+//             console.log("Buzz");
+//         }else if (i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         }else {
+//             console.log(i);
+//         }
+//     }
+// }
+// fizzBuzz();
+
+
+//Walkthrough
+(() => {
+    const fizzBuzz = numTil => {
+        for (let x = 1; x <= numTil; x++) {
+            if (x % 3 === 0 && x % 5 === 0) {
+                console.log("FizzBuzz");
+            }else if (x % 3 === 0) {
+                console.log("Fizz");
+            }else if (x % 5 === 0) {
+                console.log("Buzz");
+            }else {
+                console.log(x);
+            }
+        }
+    }
+    fizzBuzz(30)
+})();
